f20f08183d84a14dea25126484ab7e3da98857a4
codelyss <atcodeveloper@gmail.com>
2020-05-02 14:50:46 -0400

    Added endpoints to new users.

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ const responseController = require('./controllers/responseController.js');
 const PORT = process.env.PORT || 1332;
 const app = express();
 
+app.use(express.static('views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/main.html'));
+});
+
+app.get('/newuser', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/newuser.html'));
+});
+
+app.post('/newuser', (req, res) => {
+    var data = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    userController.createUser(data).then(result => {
+        res.send(result);
+    });
 });
 
 app.listen(PORT, () => {
@@ -18,4 +37,5 @@ app.listen(PORT, () => {
         console.log(`API server now on port ${PORT}!`);
     });
 });
+
 
