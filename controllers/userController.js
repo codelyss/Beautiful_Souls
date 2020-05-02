b7e0186c17d45b08799ad55e7c4f441b22f85c6d
codelyss <atcodeveloper@gmail.com>
2020-05-02 14:50:15 -0400

    Added bcrypt npm and added code to usercontroller that creates new users.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,17 @@
 const users = require('../models/user.js');
 const { Op } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const createUser = async function(data) {
+    var salt = await bcrypt.genSalt(10);
+    var encryptedPassword = await bcrypt.hash(data.password, salt);
+    const result = await users.create({
+        username: data.username,
+        password: encryptedPassword,
+        active: true,
+        reported_count: 0
+    });
+    return result;
+}
+
+module.exports = {createUser};
