dc8c25d9856162ad81643ad41ee178b20566372c
codelyss <atcodeveloper@gmail.com>
2020-05-03 14:25:00 -0400

    Added passport authentication to server.js

--- server.js
+++ server.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const path = require('path');
 const sequelize = require('./config/connection.js');
+const session = require('express-session');
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 const userController = require('./controllers/userController.js');
 const letterController = require('./controllers/letterController.js');
 const responseController = require('./controllers/responseController.js');
 
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
+
 const PORT = process.env.PORT || 1332;
 const app = express();
 
 app.use(express.static('views'));
+app.use(session({
+    secret: 'beautiful souls secret message here',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } //this should be turned to true before deploying to heroku
+}));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    userController.getUserById(id).then(result => {
+        done(null, result.id);
+    });
+});
+
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        userController.getUserByUsername(username).then(result => {
+            if (result == null) { return done("User not found"); }
+            if (result.validPassword(password)) {
+                return done(null, result);
+            }
+        });
+    }
+));
+
+app.get('/', ensureLoggedIn('/login'), (req, res) => {
     res.sendFile(path.join(__dirname, './views/main.html'));
 });
 
@@ -25,17 +60,11 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
-app.post('/login', (req, res) => {
-    var username = req.body.username;
-    userController.getUserByUsername(username).then(result => {
-        if (result != null) {
-            res.redirect('/');
-        }
-        else {
-            res.send("No user found.");
-        }
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/');
     });
-});
 
 app.post('/newuser', (req, res) => {
     var data = {
