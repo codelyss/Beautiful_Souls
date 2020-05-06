dc03892b89f42e615eecc1b9ad5106b4be48a947
codelyss <atcodeveloper@gmail.com>
2020-05-06 18:00:28 -0400

    Fixed the saving function.

--- controllers/letterController.js
+++ controllers/letterController.js
@@ -1,11 +1,11 @@
 const letters = require('../models/letter.js');
+const User = require('../models/user.js');
 const { Op } = require('sequelize');
 
 const createLetter = async function (userid, data) {
     const result = await letters.create({
-        message: data.message
-    }).then(newletter => {
-        newletter.setUser(userid);
+        message: data.message,
+        UserId: userid
     });
     return result;
 }
