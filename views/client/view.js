939493f3ee0945cda0221dfdcd2971e6f6fb7872
codelyss <atcodeveloper@gmail.com>
2020-05-09 14:00:53 -0400

    Created starter view page and js function to retrieve initial and next letter.

--- views/client/view.js
+++ views/client/view.js
@@ -1,16 +1,59 @@
+let currentLetterId;
+
 function showLetters() {
     $.ajax({
-        url: '/api/viewLetters',
+        url: '/api/viewRecentLetter',
         type: 'GET'
-    }).then(letters => {
-        letters.forEach(letter => {
-            $('<div />', {
-                html: letter.message,
-            }).click(function () {
-                alert(letter.id);
-            }).appendTo('#divLetters');
-        });
+    }).then(letter => {
+        if (letter == '') {
+            //show message to user that there aren't any letters to show.
+            //and disable next button
+            $('#btnNextLetter').prop('disabled', true);
+            return;
+        }
+
+        displayLetter(letter);
     });
 }
 
+function showNextLetter() {
+    $.ajax({
+        url: '/api/viewNextLetter/' + currentLetterId,
+        type: 'GET'
+    }).then(letter => {
+        if (letter == null) {
+            return;
+        }
+
+        displayLetter(letter);
+    });
+}
+
+function displayLetter(letter) {
+    currentLetterId = letter.id;
+    let message = letter.message;
+    $('#txtLetter').val(message);
+}
+
+
+
+
 showLetters();
+
+
+
+
+// function showLetters() {
+//     $.ajax({
+//         url: '/api/viewRecentLetter',
+//         type: 'GET'
+//     }).then(letters => {
+//         letters.forEach(letter => {
+//             $('<div />', {
+//                 html: letter.message,
+//             }).click(function () {
+//                 alert(letter.id);
+//             }).appendTo('#divLetters');
+//         });
+//     });
+// }
