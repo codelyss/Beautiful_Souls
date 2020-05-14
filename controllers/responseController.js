bb993e4df0bedf373bb5a381d36a9a3679196b0b
codelyss <atcodeveloper@gmail.com>
2020-05-14 15:05:03 -0400

    Added multiple new functions to the response controller.

--- controllers/responseController.js
+++ controllers/responseController.js
@@ -21,7 +21,78 @@ const viewMyResponses = async function (userid) {
     return result;
 }
 
+const viewRecentResponse = async function (userid, responseuserid, letterid) {
+    const result = await responses.findOne({
+        where: {
+            [Op.or]: [
+                {UserId: userid},
+                {UserId: responseuserid}
+            ],
+            LetterId: letterid
+        },
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    });
+    return result;
+}
+
+const viewNextResponse = async function (letterid, responseid) {
+     const result = await responses.findOne({
+        where: {
+            id: {
+                [Op.lt]: responseid
+            },
+            LetterId: {
+                [Op.eq]: letterid
+            }
+        },
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    });
+
+    if (result == null) {
+        if (responseid == Number.MAX_SAFE_INTEGER) {
+            return null;
+        }
+        responseid = Number.MAX_SAFE_INTEGER;
+        return viewNextResponse(letterid, responseid);
+    }
+
+    return result;
+}
+
+const viewPreviousResponse = async function (letterid, responseid) {
+    const result = await responses.findOne({
+        where: {
+            id: {
+                [Op.gt]: responseid
+            },
+            LetterId: {
+                [Op.eq]: letterid
+            }
+        },
+        order: [
+            ['createdAt', 'ASC']
+        ]
+    });
+
+    if (result == null) {
+        if (responseid == Number.MIN_SAFE_INTEGER) {
+            return null;
+        }
+        responseid = Number.MIN_SAFE_INTEGER;
+        return viewPreviousResponse(letterid, responseid);
+    }
+
+    return result;
+}
+
 module.exports = {
     createResponse,
-    viewMyResponses
+    viewMyResponses,
+    viewRecentResponse,
+    viewNextResponse,
+    viewPreviousResponse
 };
